0bfeb6a98e35cd1b6945dcdfa4b1be5bc6f6c87e
0187773933 <collincerbus@student.olympic.edu>
2021-06-01 06:01:03 -0400

    9

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # sudo docker rm public-homebridge -f || echo ""
-sudo docker build -t public-homebridge .
-#sudo docker run -it public-homebridge
-sudo docker run -it \
---net=host \
-public-homebridge
-
-# --mount type=bind,source="$(pwd)"/config,target=/home/morphs/.homebridge \
-# sudo docker run -dit --restart='always' \
-# --name public-homebridge \
+# sudo docker build -t public-homebridge .
+# sudo docker run -it \
 # --net=host \
-# --mount type=bind,source="$(pwd)"/config,target=/home/morphs/.homebridge \
 # public-homebridge
 
-# --privileged
-# -p 8581:8581 \
+id=$(sudo docker run -dit --restart='always' \
+--name public-homebridge \
+--net=host \
+public-homebridge)
+echo $id
+sudo docker logs -f $id
